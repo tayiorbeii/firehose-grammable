3084bff67e58a7c8a732ead228ed176c8871b892
Taylor Bell <taylorbell@gmail.com>
2018-02-17 15:05:47 -0700

    gram specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,7 +3,12 @@ FactoryBot.define do
     sequence :email do |n|
       "dummyEmail#{n}@gmail.com"
     end
-    password "secretPassword"
-    password_confirmation "secretPassword"
+    password 'secretPassword'
+    password_confirmation 'secretPassword'
+  end
+
+  factory :gram do
+    message 'hello'
+    association :user
   end
 end
