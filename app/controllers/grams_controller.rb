981dd327d57f2ca1ba8f9cc2a8afb53e415d169c
Taylor Bell <taylorbell@gmail.com>
2018-02-18 21:20:09 -0700

    controller refactor

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -57,7 +57,7 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
+  def render_not_found(status = :not_found)
+    render plain: 'Not Found :(', status: status
   end
 end
