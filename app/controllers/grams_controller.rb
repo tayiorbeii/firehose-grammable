3354030c643fc90419b58985b55c8e9ce31d78a9
Taylor Bell <taylorbell@gmail.com>
2018-02-19 09:49:18 -0700

    more spec

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -57,8 +57,4 @@ class GramsController < ApplicationController
   def gram_params
     params.require(:gram).permit(:message, :picture)
   end
-
-  def render_not_found(status = :not_found)
-    render plain: 'Not Found :(', status: status
-  end
 end
